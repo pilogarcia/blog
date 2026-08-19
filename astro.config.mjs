69f7331dbbc39a488bbdbdd49d8a5f1728f9b381
Pilo García <pilogarcia@gmail.com>
2026-08-19 11:17:20 -0300

    cambios en archivos

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://elojoinusual.com.ar',
-  integrations: [sitemap()],
+  integrations: [sitemap(), mdx()],
 });
